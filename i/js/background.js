b310fe11f052ce6bfc5139f6bf57e636fb909236
adrien <adrien@adrien-desktop>
2015-01-13 15:09:49 +0100

    uri en local storage

--- i/js/background.js
+++ i/js/background.js
@@ -1,7 +1,15 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        id : "main"
-    }, function(createdWindow) {
-        //createdWindow.fullscreen();
-        });
+    
+    chrome.storage.local.get("uri", function(item) {
+        if(!item.uri) {
+            chrome.storage.local.set({
+                "uri":"ws://dev.adrien.office:57570"
+            });
+        }
+        chrome.app.window.create('index.html', {
+            id : "main"
+        }, function(createdWindow) {
+            //createdWindow.fullscreen();
+            });
+    });
 });
--- i/js/tcpclient.js
+++ i/js/tcpclient.js
@@ -1,50 +1,64 @@
 TcpClient = function() {
-    var self = this;
     this.notificationId = 0;
     this.queryId=0;
-    this.socket = new WebSocket("ws://dev.adrien.office:57570");
-    this.socket.onopen = function() {
-        self.onOpenCallback();
-    };
-    this.socket.onmessage = function(msg) {
-        self.onMessageCallback(msg);
-    };
-    this.socket.onclose = function() {
-        self.onCloseCallback();
-    };
-    this.socket.onerror = function(event) {
-        self.onErrorCallback(event);
-    };
+    this._setConfig();
 }
 
 TcpClient.prototype = {
-    onOpenCallback : function() {
-        //Hide the socket connect window
-        clearTimeout(this.timeout);
-        $(".splashscreen").hide();
-    },
-    onMessageCallback : function(msg) {
-        notification = new Notification(msg.data+" "+this.notificationId++);
-        notification.message();
-    },
-    onCloseCallback : function() {
-        //Show the socket connect window
-        $(".splashscreen").show();
-        this.timeout = setTimeout(function(){
-            tcpClient = new TcpClient();
-        },3000);
-    },
-    onErrorCallback : function(event) {
-    },
-    incrementQueryId: function() {
-        this.queryId++;  
+    connect: function() {
+        var that = this;
+        this.socket = new WebSocket(this.uri);
+        this.socket.onopen = function() {
+            that._onOpenCallback();
+        };
+        this.socket.onmessage = function(msg) {
+            that._onMessageCallback(msg);
+        };
+        this.socket.onclose = function() {
+            that._onCloseCallback();
+        };
+        this.socket.onerror = function(event) {
+            that._onErrorCallback(event);
+        };
     },
     notify: function(type, value) {
         this.socket.send("<notify type='"+type+"' value='"+value+"'/>");
     },
     request: function(type) {
-        this.incrementQueryId();
+        this._incrementQueryId();
         this.socket.send("<request type='"+type+"' id='"+this.queryId+"'/>");
+    },
+    _setConfig: function() {
+        var that = this;
+        chrome.storage.local.get("uri",function(item){
+            if(item.uri) {
+                that.uri = item.uri;
+            }
+            that.connect();
+        });
+    },
+    _onOpenCallback : function() {
+        //Hide the socket connect window
+        clearTimeout(this.timeout);
+        $(".splashscreen").hide();
+    },
+    _onMessageCallback : function(msg) {
+        notification = new Notification(msg.data+" "+this.notificationId++);
+        notification.message();
+    },
+    _onCloseCallback : function() {
+        //Show the socket connect window
+        var that = this;
+        $(".splashscreen").show();
+        this.timeout = setTimeout(function(){
+            tcpClient = that.connect();
+        },3000);
+    },
+    _onErrorCallback : function(event) {
+    //console.log(event);
+    },
+    _incrementQueryId: function() {
+        this.queryId++;  
     }
     
 }
