f195a10eb5ea823c1a6cee17f4e0590e23efb053
Michel <michel@recisio.com>
2015-02-04 13:56:41 +0100

    Video Background + animations

--- i/js/app.js
+++ i/js/app.js
@@ -1,9 +1,10 @@
 var tcpClient;
 var settings;
+var eff_timeout;
 $(document).ready(function () {
-    settings =  new Settings();
-    setTimeout(function() {
-        if(settings.isReady) {
+    //settings = new Settings();
+    setTimeout(function () {
+        if (settings.isReady = 1) {
             tcpClient = new TcpClient(settings);
             tcpClient.connect();
             player = new Player();
@@ -14,4 +15,22 @@ $(document).ready(function () {
             clearTimeout();
         }
     }, 1000);
+
+    $('body').on('mouseup', '.click_feedback', function () {
+        $(this).addClass('clicked');
+        clearTimeout(eff_timeout);
+        eff_timeout = setTimeout(function () {
+            $('.click_feedback').removeClass('clicked');
+        }, 500);
+    }).on('mousedown', '.lift', function () {
+        var me = $(this);
+        eff_timeout = setTimeout(function () {
+            me.addClass('lifted');
+        }, 500);
+    }).on('mouseup', '.lift', function () {
+        $(this).removeClass('lifted');
+    }).on('mouseup', '.song_card', function () {
+        $(this).children('.card__popup').css('display', 'initial').addClass('visible');
+    });
+
 });
